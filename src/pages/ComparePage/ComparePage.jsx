dfdef0d7b2c4348c1b6da8cf8979bdb7b68949bc
Gercogor <gercogor@gmail.com>
2022-05-24 12:04:22 +0700

    develop

--- src/pages/ComparePage/ComparePage.jsx
+++ src/pages/ComparePage/ComparePage.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import "./ComparePage.css"
-import CompareModal from "../../UI/CompareModal/CompareModal";
+import React, { useState } from 'react';
 import MyResult from '../../json/products.json';
+import CompareModal from "../../UI/CompareModal/CompareModal";
+import "./ComparePage.css";
 
 
 const ComparePage = () => {
@@ -9,7 +9,7 @@ const ComparePage = () => {
     const [visible, setVisible] = useState(false);
     const [count, setCount] = useState(2)
 
-    function handleCount (e) {
+    function handleCount(e) {
         setCount(+e.target.innerHTML);
         let prod = [];
         for (let i = 0; i < +e.target.innerHTML; i++) {
@@ -20,32 +20,62 @@ const ComparePage = () => {
 
     return (
         <div className="compare">
-            <button onClick={()=>console.log(products)}>!@@!</button>
+            <button onClick={() => console.log(products)}>!@@!</button>
             <header className="compare__header">
                 <h1 className="compare__header-title">SmartPhones</h1>
                 <div className="compare__header-num-change">
                     <p>Отобразить товаров:</p>
                     <ul className="compare__header-count">
-                        <li onClick={event=>handleCount(event)}>2</li>
-                        <li onClick={event=>handleCount(event)}>3</li>
-                        <li onClick={event=>handleCount(event)}>4</li>
-                        <li onClick={event=>handleCount(event)}>5</li>
-                        <li onClick={event=>handleCount(event)}>6</li>
+                        <li onClick={event => handleCount(event)}>2</li>
+                        <li onClick={event => handleCount(event)}>3</li>
+                        <li onClick={event => handleCount(event)}>4</li>
+                        <li onClick={event => handleCount(event)}>5</li>
+                        <li onClick={event => handleCount(event)}>6</li>
                     </ul>
                 </div>
             </header>
-            <main>
-                <table>
-                    {
-                        MyResult.ParametricList_Attributes.map(item=>
-                            <tr>{item.text}</tr>
-                        )
-                    }
+            <main className="compare__main">
+                <table className='compare__table'>
+                    <tbody className='compare__table-body'>
+                        <tr className='table__row'>
+                            <td className='table__row-head table__row-cell_border'></td>
+                            {products.map(item =>
+                                <td className='table__row-cell table__row-cell_border'>
+                                    <div className='table__item'>
+                                        <img className="table__pic" src={item.url} alt="" />
+                                        <CompareModal visible={visible} />
+                                    </div>
+                                </td>
+                            )}
+                        </tr>
+
+                        <tr className='table__row'>
+                            <td className='table__row-head'>Model</td>
+                            {products.map(item =>
+                                <td className='table__row-cell'>{item.Model}</td>
+                            )}
+                        </tr>
+
+                        <tr className='table__row'>
+                            <td className='table__row-head'>Screen</td>
+                            {products.map(item =>
+                                <td className='table__row-cell'>{item.Screen}</td>
+                            )}
+                        </tr>
+
+                        <tr className='table__row'>
+                            <td className='table__row-head'>Resolution</td>
+                            {products.map(item =>
+                                <td className='table__row-cell'>{item.Resolution}</td>
+                            )}
+                        </tr>
+
+                    </tbody>
                 </table>
             </main>
 
 
-            <CompareModal visible={visible}/>
+            <CompareModal visible={visible} />
         </div>
     );
 };
